52afd92db1bfc257584e1dc9e4f609fb7caadab3
Matthew Cech <contact@matthewcech.com>
2019-04-06 23:41:06 -0700

    + Added poll results, poll show, poll vote, rating, role

--- src/commands/CommandPollVote.java
+++ src/commands/CommandPollVote.java
@@ -8,7 +8,7 @@ public class CommandPollVote extends Command
 	public CommandPollVote(KittyRole level, KittyRating rating) { super(level, rating); }
 	
 	@Override
-	public String HelpText() { return "Vote in a poll with the choice number, won't work if no poll is running, you can't change your vote once you have cast it! Be careful!"; }
+	public String HelpText() { return Localizer.Stub("Vote in a poll with the choice number, won't work if no poll is running, you can't change your vote once you have cast it! Be careful!"); }
 	
 	@Override
 	public void OnRun(KittyGuild guild, KittyChannel channel, KittyUser user, UserInput input, Response res)
@@ -17,7 +17,7 @@ public class CommandPollVote extends Command
 		{
 			if(guild.hasVoted.contains(user.uniqueID))
 			{
-				res.Call("You already voted");
+				res.Call(Localizer.Stub("You already voted"));
 				return;
 			}
 			try 
@@ -25,21 +25,22 @@ public class CommandPollVote extends Command
 				int voteNum = Integer.parseInt(input.args)-1;
 				if(voteNum >= guild.choices.size() || voteNum < 0)
 				{
-					res.Call(voteNum + " That's not a vaild vote!");
+					res.Call(String.format(Localizer.Stub("%s That's not a vaild vote!"), voteNum));
 					return;
 				}
+				
 				KittyPoll polled = guild.choices.get(voteNum);
 				polled.votes++;
 				guild.hasVoted.add(user.uniqueID);
-				res.Call("You successfully voted for `" + polled.choice + "`!");
+				res.Call(Localizer.Stub("You successfully voted for") + " `" + polled.choice + "`!");
 				return;
 			}
 			catch (NumberFormatException e)
 			{
-				res.Call("That's not a vaild number!");
+				res.Call(Localizer.Stub("That's not a vaild number!"));
 				return;
 			}
 		}
-		res.Call("There is no poll running!");
+		res.Call(Localizer.Stub("There is no poll running!"));
 	}
 }
