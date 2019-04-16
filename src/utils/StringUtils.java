91e941e69a217a85a9b1cc5fba64bbc83c5821bc
Matthew Cech <contact@matthewcech.com>
2019-04-15 23:31:33 -0700

    = Iterated

--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -10,8 +10,20 @@ public class StringUtils
 		str = str.replaceAll("\\\\r", "\r");
 		str = str.replaceAll("\\\\f", "\f");
 		str = str.replaceAll("\\\\\"", "\"");
-		str = str.replaceAll("\\\\\\\\", "\\");
+		str = str.replaceAll("\\\\\\\\\\\\", "\\\\");
 		
 		return str;
 	}
+	
+	public static String ReEscape(String str)
+	{
+		str = str.replaceAll("\\\b", "\\\\\\b");
+		str = str.replaceAll("\\\n", "\\\\\\n");
+		str = str.replaceAll("\\\t", "\\\\\\t");
+		str = str.replaceAll("\\\r", "\\\\\\r");
+		str = str.replaceAll("\\\f", "\\\\\\f");
+		
+		return str;
+	}
+	
 }
