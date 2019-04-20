125e397a10bae3e7c00032713bc393db48020102
Matthew Cech <contact@matthewcech.com>
2019-04-20 02:08:38 -0700

    + Added example plugin

---
+++ plugins/example.lua
@@ -0,0 +1,6 @@
+-- Example plugin stub. This function is called and passed the input message.
+-- If nil is returned, the plugin is considered to have not run, but if any 
+-- other value is returned, it's interpreted as a string and sent back to the user.
+function plugin(message)
+	return nil;
+end
