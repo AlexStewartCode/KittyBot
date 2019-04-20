25c1e9a1a2ffb931153278e744756b273ff36e8d
Matthew Cech <contact@matthewcech.com>
2019-04-20 00:22:39 -0700

    + Added plugin stubs

---
+++ src/core/lua/Plugin.java
@@ -0,0 +1,8 @@
+package core.lua;
+
+// To promote flexibility, plugins are lua file with predefined callbacks.
+// They are executed between the initial built-in preprocessing and the command parsing.
+public class Plugin
+{
+	
+}
---
+++ src/core/lua/PluginManager.java
@@ -0,0 +1,8 @@
+package core.lua;
+
+// Reads in, handles, and manipulates plugins. Plugins are 
+// loaded in the order they appear in the folder.
+public class PluginManager
+{
+
+}
