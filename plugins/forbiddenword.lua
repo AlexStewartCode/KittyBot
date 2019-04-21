6d1b4aa9f3015209ec7867140eef9a58f5c228cf
Matthew Cech <contact@matthewcech.com>
2019-04-20 23:42:11 -0700

    = Fixed conditional

--- plugins/forbiddenword.lua
+++ plugins/forbiddenword.lua
@@ -1,9 +1,11 @@
 local time = os.time();
+
 function plugin(message) 
-	if(message.match("purple"))then
+	if(string.match(message, "purple"))then
 		local response = os.time() - time;
 		time = os.time();
 		return response;
 	end
+	
 	return nil;
 end
