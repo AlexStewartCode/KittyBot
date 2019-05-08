faa2ef567129d608c5b0f1a4412b3f2e777f89c7
Alex <github@alexstewart.app>
2019-05-08 01:16:35 -0400

    +Added mentioning capability to beanshowing

--- src/commands/CommandBeansShow.java
+++ src/commands/CommandBeansShow.java
@@ -14,6 +14,16 @@ public class CommandBeansShow extends Command
 	@Override
 	public void OnRun(KittyGuild guild, KittyChannel channel, KittyUser user, UserInput input, Response res)
 	{
-		res.Call(String.format(LocStrings.Stub("BeansShowDisplay"), user.GetBeans()));
+		if(input.mentions == null)
+			res.Call(String.format(LocStrings.Stub("BeansShowDisplay"), user.GetBeans()));
+		else
+		{
+			String mentionedBeans = ""; 
+			for(KittyUser mentioned:input.mentions)
+			{
+				mentionedBeans += mentioned.name + " has " + mentioned.GetBeans() + " ";
+			}
+			res.Call(String.format(LocStrings.Stub("BeansShowMentioned"), mentionedBeans));
+		}
 	}
 }
