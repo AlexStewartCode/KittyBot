d6aa2232c8463dc4a45a8f281527122074fa05d2
Matthew Cech <contact@matthewcech.com>
2019-06-17 23:20:47 -0700

    = Classpath update

--- src/network/NetworkTheColorAPI.java
+++ src/network/NetworkTheColorAPI.java
@@ -2,12 +2,15 @@ package network;
 
 import com.google.gson.Gson;
 
+import network.NetworkE621.E621ResponseObject;
+import utils.HTTPUtils;
+
 // This class is designed to easily query and ask for responses from https://www.thecolorapi.com/.
 // Unlike some of the other networking classes, the query is much more wrapped.
 @SuppressWarnings("unused")
 public class NetworkTheColorAPI
 {
-	private static final Gson jsonParser_ = new Gson();
+	private static final Gson jsonParser = new Gson();
 	
 	private class tca_hsv
 	{
@@ -35,11 +38,19 @@ public class NetworkTheColorAPI
 		public String clean; // format xxxxxx, with no #
 	}
 	
-	private class tca_obj
+	public class ColorData
 	{
 		public tca_hex hex;
 		public tca_rgb rgb;
 		public tca_hsl hsl;
 		public tca_hsv hsv;
+	}
+	
+	public ColorData LookupHex()
+	{
+		String response = HTTPUtils.SendPOSTRequest("https://www.thecolorapi.com/id?hex=24B1E0");
+		
+		// Use class evaluation on an array of the response imageObject to be able to hold multiple.
+		ColorData imageObj = jsonParser.fromJson(response, ColorData.class);
 	}
 }
