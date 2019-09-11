770ed84983bcc0a06c752da545f5f3916d691719
Matthew Cech <contact@matthewcech.com>
2019-09-11 13:58:46 -0700

    + Add dew base

---
+++ src/commands/dew/CommandDew.java
@@ -0,0 +1,28 @@
+package commands.dew;
+
+import core.Command;
+import core.SubCommandFramework;
+import dataStructures.KittyChannel;
+import dataStructures.KittyGuild;
+import dataStructures.KittyRating;
+import dataStructures.KittyRole;
+import dataStructures.KittyUser;
+import dataStructures.Response;
+import dataStructures.UserInput;
+
+public class CommandDew extends Command  {
+
+	SubCommandFramework framework = new SubCommandFramework();
+	public CommandDew(KittyRole level, KittyRating rating) 
+	{
+		super(level, rating);
+		
+		framework.addCommand("start", command);
+	}
+	
+	@Override
+	public void onRun(KittyGuild guild, KittyChannel channel, KittyUser user, UserInput input, Response res) 
+	{
+		framework.run(guild, channel, user, input).Call(res);
+	}
+}
